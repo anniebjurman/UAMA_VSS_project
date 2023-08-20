c26f20621577b470aec7d6cd2f7b862cf7de03bd
Annie Bjurman <id19abn@cs.umu.se>
2023-08-20 12:25:20 +0200

    display beer name frag on click

--- app/src/main/java/se/umu/cs/id19abn/upg3/MainActivity.kt
+++ app/src/main/java/se/umu/cs/id19abn/upg3/MainActivity.kt
@@ -3,22 +3,30 @@ package se.umu.cs.id19abn.upg3
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nextButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (savedInstanceState == null) {
-            this.displayHomeFragment()
+            displayHomeFragment()
         }
 
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
+
+        nextButton = findViewById(R.id.btn_next_step)
+        nextButton.setOnClickListener { view: View ->
+            displayBeerNameFragment()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -29,7 +37,14 @@ class MainActivity : AppCompatActivity() {
     private fun displayHomeFragment() {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
-            add<Home>(R.id.fragment_container_view)
+            add<HomeFragment>(R.id.fragment_container_view)
+        }
+    }
+
+    private fun displayBeerNameFragment() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<BeerNameFragment>(R.id.fragment_container_view)
         }
     }
 }
