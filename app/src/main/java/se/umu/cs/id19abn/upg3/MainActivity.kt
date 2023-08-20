333de617a021c1e804156169fbe7b87995f084d0
Annie Bjurman <id19abn@cs.umu.se>
2023-08-20 15:12:15 +0200

    add logo to tool bar + updates

--- app/src/main/java/se/umu/cs/id19abn/upg3/MainActivity.kt
+++ app/src/main/java/se/umu/cs/id19abn/upg3/MainActivity.kt
@@ -5,27 +5,46 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.View
 import android.widget.Button
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.lifecycle.Observer
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var nextButton: Button
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var beerNameFragment: Fragment
+    private lateinit var fm: FragmentManager
+    private val viewModel: ItemViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        viewModel.selectedItem.observe(this, Observer { item ->
+            // Perform an action with the latest item data.
+        })
+
+        // fragment variables
+        homeFragment = HomeFragment()
+        beerNameFragment = BeerNameFragment()
+        fm = supportFragmentManager
+
         if (savedInstanceState == null) {
-            displayHomeFragment()
+            displayFragment(homeFragment)
         }
 
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.setIcon(R.drawable.logo);
 
         nextButton = findViewById(R.id.btn_next_step)
         nextButton.setOnClickListener { view: View ->
-            displayBeerNameFragment()
+            displayFragment(beerNameFragment)
         }
     }
 
@@ -34,17 +53,21 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    private fun displayHomeFragment() {
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            add<HomeFragment>(R.id.fragment_container_view)
-        }
+    private fun displayFragment(frag: Fragment) {
+        fm.beginTransaction().add(R.id.fragment_container_view, frag).commit()
     }
 
-    private fun displayBeerNameFragment() {
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            add<BeerNameFragment>(R.id.fragment_container_view)
-        }
-    }
+//    private fun displayHomeFragment() {
+//        supportFragmentManager.commit {
+//            setReorderingAllowed(true)
+//            add<HomeFragment>(R.id.fragment_container_view)
+//        }
+//    }
+//
+//    private fun displayBeerNameFragment() {
+//        supportFragmentManager.commit {
+//            setReorderingAllowed(true)
+//            add<BeerNameFragment>(R.id.fragment_container_view)
+//        }
+//    }
 }
