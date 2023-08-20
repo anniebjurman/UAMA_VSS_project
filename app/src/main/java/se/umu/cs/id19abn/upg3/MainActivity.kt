45cf7306f3aaae608c15130d930def7b8e1f8387
Annie Bjurman <id19abn@cs.umu.se>
2023-08-20 11:22:07 +0200

    add home fragment

--- app/src/main/java/se/umu/cs/id19abn/upg3/MainActivity.kt
+++ app/src/main/java/se/umu/cs/id19abn/upg3/MainActivity.kt
@@ -4,18 +4,32 @@ package se.umu.cs.id19abn.upg3
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            this.displayHomeFragment()
+        }
+
         setContentView(R.layout.activity_main)
-        // Note that the Toolbar defined in the layout has the id "my_toolbar"
         setSupportActionBar(findViewById(R.id.my_toolbar))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun displayHomeFragment() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<Home>(R.id.fragment_container_view)
+        }
     }
 }
