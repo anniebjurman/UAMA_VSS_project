1ba35c08e1262abdcf1b80b3a502fbde6ca0e621
Annie Bjurman <id19abn@cs.umu.se>
2023-09-04 18:01:06 +0200

    add popup

--- app/src/main/java/se/umu/cs/id19abn/upg3/HomeFragment.kt
+++ app/src/main/java/se/umu/cs/id19abn/upg3/HomeFragment.kt
@@ -1,11 +1,17 @@
 package se.umu.cs.id19abn.upg3
 
+import android.R
+import android.annotation.SuppressLint
+import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import se.umu.cs.id19abn.upg3.databinding.FragmentHomeBinding
@@ -18,6 +24,7 @@ class HomeFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,6 +36,26 @@ class HomeFragment : Fragment() {
             val beerGame = BeerGame()
             val action = HomeFragmentDirections.actionHomeFragmentToBeerNameFragment(beerGame)
             binding.root.findNavController().navigate(action)
+        }
+
+        binding.btnInfo.setOnClickListener {
+            val popupView = LayoutInflater.from(activity).inflate(se.umu.cs.id19abn.upg3.R.layout.game_information, container, false)
+
+            // create the popup window
+            val width = LinearLayout.LayoutParams.WRAP_CONTENT
+            val height = LinearLayout.LayoutParams.WRAP_CONTENT
+            val focusable = true // lets taps outside the popup also dismiss it
+            val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+            // show the popup window
+            // which view you pass in doesn't matter, it is only used for the window tolken
+            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+
+            // dismiss the popup window when touched
+            popupView.setOnTouchListener { _, _ ->
+                popupWindow.dismiss()
+                true
+            }
         }
         return binding.root
     }
